c3c3d2831874d3c97a5abbd53d3cad6188bfc328
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-27 15:01:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,9 +35,9 @@ def update_counting_hash(hash, key)
   update_counting_hash = {
     hash => key
   }
-  if update_counting_hash[key]
-    update_counting_hash[key] += 1
+  if hash[key]
+    hash[key] += 1
   else
-    update_counting_hash[key] = 1
+    hash[key] = 1
   end
 end
