dea0b45983d2da76d8746225cd49cb843478af93
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-27 14:59:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,9 +32,12 @@ def read_from_hash(hash,key)
 end
 
 def update_counting_hash(hash, key)
+  update_counting_hash = {
+    hash => key
+  }
   if update_counting_hash[key]
     update_counting_hash[key] += 1
   else
-    hash[key] = 1
+    update_counting_hash[key] = 1
   end
 end
