51743b2ef13bb2ea3ddfb694e7206d1850882d04
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-27 14:47:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,10 +27,8 @@ def my_hash_creator(key, value)
   }
 end
 
-def read_from_hash(hash, key)
-  {
-    hash => key
-  }
+def read_from_hash(hash,key)
+  read_from_hash[key]
 end
 
 def update_counting_hash(hash, key)
